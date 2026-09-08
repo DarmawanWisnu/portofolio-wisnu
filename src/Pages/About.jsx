0b3108824e4b2335afe0402f2fa00acfbea4609d
klerie dev <wisnudarmawan.dev@gmail.com>
2026-09-08 12:56:36 +0700

    change year

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -137,15 +137,16 @@ const AboutPage = () => {
       const storedProjects = JSON.parse(localStorage.getItem("projects") || "[]");
       const storedCertificates = JSON.parse(localStorage.getItem("certificates") || "[]");
       
-      const startDate = new Date("2026-03-01");
-      const today = new Date();
-      const experience = today.getFullYear() - startDate.getFullYear() -
-        (today < new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate()) ? 1 : 0);
+      // const startDate = new Date("2026-03-01");
+      // const today = new Date();
+      // const experience = today.getFullYear() - startDate.getFullYear() -
+      //   (today < new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate()) ? 1 : 0);
 
       setStats({
         totalProjects: storedProjects.length,
         totalCertificates: storedCertificates.length,
-        YearExperience: experience
+        // YearExperience: experience
+        YearExperience: 2
       });
     };
 
